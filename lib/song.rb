2436300449e8a6db2814698257251c55cd5d3e25
sir-udenna <udennaboy@gmail.com>
2021-03-04 16:50:47 -0600

    commit tree 5a49a1db813627bc9cb99e0e474d648fd003014e

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ require 'pry'
 class Song
   attr_accessor :name
   attr_reader :artist
+  extend Memorable
 
   @@songs = []
 
@@ -18,13 +19,13 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def artist=(artist)
     @artist = artist
